c297c09d24d810dea12a2fc75da5354c57b1e9fc
neeleshrohit <87979871+neeleshrohit@users.noreply.github.com>
2023-02-23 11:23:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src="https://cdn.w600.comps.canstockphoto.com/autumn-welcome-banner-wood-background-image_csp61429575.jpg">
+        
 <h3>React /node project</h3>
       </header>
     </div>
