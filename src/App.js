712fa4b589bcc25755b58c2aa57b8151458c39dd
neeleshrohit <87979871+neeleshrohit@users.noreply.github.com>
2023-02-23 11:02:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <img src="https://thumbs.dreamstime.com/b/welcome-banner-colorful-serpentine-welcome-banner-spruce-branches-colorful-paper-serpentine-vector-illustration-103884659.jpg" >
+        <img src="https://thumbs.dreamstime.com/b/welcome-banner-colorful-serpentine-welcome-banner-spruce-branches-colorful-paper-serpentine-vector-illustration-103884659.jpg" alt="Flowers in Chania">
         
 <h3>React & node project</h3>
       </header>
