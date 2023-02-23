96579c35f05980cecffaf612db2a7ccfefb0047e
neeleshrohit <87979871+neeleshrohit@users.noreply.github.com>
2023-02-23 11:29:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <img src="https://phoneky.co.uk/thumbs/screensavers/down/new/signs/welcome_vYPgtPqQ.gif" />
 <h3>React /node project</h3>
       </header>
     </div>
